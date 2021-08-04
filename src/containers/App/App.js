6d40750bf44a40eecb89947c31e9a492e00696e2
Andrii Kochmaruk <luckyanry@gmail.com>
2021-08-04 10:07:49 +0200

    changed webpack.config.js added DevExtreme alias config for alias[329]

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 
-// import CountriesList from "../../components/CountriesList/CountriesList";
+import CountriesList from "../../components/CountriesList/CountriesList";
 // import TestDevEx from "../../components/TestDevEx/TestDevEx";
 
 import classes from "./App.module.css";
@@ -10,7 +10,7 @@ export default class App extends Component {
     return (
       <div className={classes.appWrapper}>
         <h1 className={classes.appTitle}>Countries</h1>
-        {/* <CountriesList /> */}
+        <CountriesList />
         {/* <TestDevEx /> */}
       </div>
     );
