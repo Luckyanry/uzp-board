68785c3299f61373cba83a64606a964f8d9bf731
Andrii Kochmaruk <luckyanry@gmail.com>
2021-08-05 11:54:57 +0200

    added fetch data for CountriesList component

--- src/components/CountriesList/CountriesList.js
+++ src/components/CountriesList/CountriesList.js
@@ -5,46 +5,35 @@ import DataGrid, {Column, Paging, Pager} from "devextreme-react/data-grid";
 import CustomStore from "devextreme/data/custom_store";
 import "whatwg-fetch";
 
-function isNotEmpty(value) {
-  return value !== undefined && value !== null && value !== "";
-}
-
-// const url = "http://10.0.10.71/actions.asp";
+const baseUrl = "http://10.0.10.71/actions.asp";
 
 const store = new CustomStore({
-  key: "OrderNumber",
-  load: function (loadOptions) {
-    let params = "?";
-    [
-      "skip",
-      "take",
-      "requireTotalCount",
-      "requireGroupCount",
-      "sort",
-      "filter",
-      "totalSummary",
-      "group",
-      "groupSummary",
-    ].forEach(function (i) {
-      if (i in loadOptions && isNotEmpty(loadOptions[i])) {
-        params += `${i}=${JSON.stringify(loadOptions[i])}&`;
-      }
-    });
-    params = params.slice(0, -1);
-    return fetch(
-      `https://js.devexpress.com/Demos/WidgetsGalleryDataService/api/orders${params}`
-    )
+  key: "guid",
+  load: function () {
+    let data = new URLSearchParams();
+    data.append("operation", "load");
+    data.append("db", "hbdb");
+    data.append("schema", "get");
+    data.append("sp", "Countries");
+
+    let options = {
+      method: "POST",
+      body: data,
+      headers: {
+        Accept: "application/json",
+      },
+      credentials: "include",
+    };
+
+    return fetch(`${baseUrl}`, options)
       .then((response) => {
         if (response.ok) return response.json();
-        console.log(response.json());
         throw new Error("Error fetching data");
       })
-      .then(({data, totalCount, summary, groupCount}) => {
+      .then((data) => {
         return {
           data,
-          totalCount,
-          summary,
-          groupCount,
+          totalCount: data.length,
         };
       })
       .catch((err) => {
@@ -52,17 +41,19 @@ const store = new CustomStore({
       });
   },
 });
-
 class CountriesList extends React.Component {
   render() {
     return (
-      <DataGrid dataSource={store} showBorders={true} remoteOperations={true}>
-        <Column dataField="OrderNumber" dataType="number" />
-        <Column dataField="OrderDate" dataType="date" />
-        <Column dataField="StoreCity" dataType="string" />
-        <Column dataField="StoreState" dataType="string" />
-        <Column dataField="Employee" dataType="string" />
-        <Column dataField="SaleAmount" dataType="number" format="currency" />
+      <DataGrid dataSource={store} showBorders={true}>
+        <Column dataField="short_name" dataType="string" />
+        <Column dataField="numeric" dataType="string" />
+        <Column dataField="alpha2code" dataType="string" />
+        <Column dataField="alpha3code" dataType="string" />
+        <Column dataField="short_name_eng" dataType="string" />
+        <Column dataField="short_name_karlat" dataType="string" />
+        <Column dataField="short_name_rus" dataType="string" />
+        <Column dataField="short_name_uzcyr" dataType="string" />
+        <Column dataField="short_name_uzlat" dataType="string" />
         <Paging defaultPageSize={10} />
         <Pager showPageSizeSelector={true} allowedPageSizes={[10, 20, 50]} />
       </DataGrid>
--- src/server/axios-countries.js
+++ src/server/axios-countries.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://...",
+  baseURL: "http://10.0.10.71/actions.asp",
 });
