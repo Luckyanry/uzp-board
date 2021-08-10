1db2ec730b3690273e56176c21e52356ce2a7eed
Andrii Kochmaruk <luckyanry@gmail.com>
2021-08-11 00:01:36 +0200

    changed navigation toolbar position from inner for outer

--- src/Content.js
+++ src/Content.js
@@ -2,7 +2,8 @@ import React from "react";
 import {Switch, Route, Redirect} from "react-router-dom";
 import {AppInfo} from "./app-info";
 import routes from "./app-routes";
-import {SideNavInnerToolbar as SideNavBarLayout} from "./layouts";
+// import {SideNavInnerToolbar as SideNavBarLayout} from "./layouts";
+import {SideNavOuterToolbar as SideNavBarLayout} from "./layouts";
 import {Footer} from "./components";
 import {useLocalization} from "./contexts/LocalizationContext";
 
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,2 +1,3 @@
+export {default as SideNavOuterToolbar} from "./SideNavOuterToolbar/SideNavOuterToolbar";
 export {default as SideNavInnerToolbar} from "./SideNavInnerToolbar/SideNavInnerToolbar";
 export {SingleCard} from "./SingleCard/SingleCard";
