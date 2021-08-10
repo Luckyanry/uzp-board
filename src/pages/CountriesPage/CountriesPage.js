c17c45c1ec09ae04373927439f3470beaaebf669
Andrii Kochmaruk <luckyanry@gmail.com>
2021-08-10 21:59:03 +0200

    deleted column murkup function from CountriesPage component

--- src/pages/CountriesPage/CountriesPage.js
+++ src/pages/CountriesPage/CountriesPage.js
@@ -16,79 +16,8 @@ import {countriesStore} from "../../api/countries-fetch";
 
 import "./CountriesPage.scss";
 
-const colmnsTitlesArr = [
-  "short_name_eng",
-  "short_name_karlat",
-  "short_name_rus",
-  "short_name_uzcyr",
-  "short_name_uzlat",
-];
-
 export const CountriesPage = () => {
   const {formatMessage} = useLocalization();
-
-  // const customizeText = (columnValue) => {
-  //   if (/^ *$/.test(columnValue.value) && columnValue.value.length > 0) {
-  //     console.log("empty string", columnValue);
-
-  //     const emptyString = {
-  //       ...columnValue,
-  //       value: null,
-  //     };
-
-  //     console.log("new", emptyString.value);
-
-  //     return emptyString.value;
-  //   }
-
-  //   console.log(`value:`, columnValue);
-  //   return columnValue.value;
-  // };
-
-  const columnsMarkup = colmnsTitlesArr
-    .map((columnTitle) => {
-      return `
-    <Column
-      dataField={${columnTitle}}
-      caption={${formatMessage(columnTitle)}}
-      alignment="left"
-      ${
-        columnTitle === "numeric" ||
-        columnTitle === "alpha2code" ||
-        columnTitle === "alpha3code"
-          ? `width={100}`
-          : null
-      }
-    >
-      ${
-        columnTitle === "numeric"
-          ? `<PatternRule
-              message={${formatMessage("numeric_err_message")}}
-              pattern={new RegExp("^[0-9]{0,4}$")}
-            />`
-          : null
-      }
-      ${
-        columnTitle === "alpha2code"
-          ? `<PatternRule
-              message={${formatMessage("alpha2code_err_message")}}
-              pattern={new RegExp("^[A-Z]{2}$")}
-            />`
-          : null
-      }
-      ${
-        columnTitle === "alpha3code"
-          ? `<PatternRule
-              message={${formatMessage("alpha3code_err_message")}}
-              pattern={new RegExp("^[A-Z]{3}$")}
-            />`
-          : null
-      }
-      ${columnTitle === "short_name_eng" ? `<RequiredRule />` : null}
-    </Column>
-    `;
-    })
-    .forEach((markup) => console.log(markup));
 
   return (
     <>
@@ -151,8 +80,6 @@ export const CountriesPage = () => {
           />
           <RequiredRule />
         </Column>
-
-        {/* columnsMarkup */}
 
         <Column
           dataField="short_name_eng"
