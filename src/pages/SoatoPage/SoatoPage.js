821f2438359b4b7dc54693345690b3973011b747
Andrii Kochmaruk <luckyanry@gmail.com>
2021-08-11 19:10:25 +0200

    added autoExpandAll into TreeList component attribute of SoatoPage

--- src/pages/SoatoPage/SoatoPage.js
+++ src/pages/SoatoPage/SoatoPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 import TreeList, {
   Column,
@@ -9,7 +9,9 @@ import TreeList, {
   Paging,
   Pager,
   Scrolling,
+  FilterRow,
 } from "devextreme-react/tree-list";
+// import Button from "devextreme-react/button";
 
 import {soatoData} from "../../api/soato-fetch";
 import {useLocalization} from "../../contexts/LocalizationContext";
@@ -17,49 +19,61 @@ import {useLocalization} from "../../contexts/LocalizationContext";
 import "./SoatoPage.scss";
 
 export const SoatoPage = () => {
+  // const [toggler, setToggler] = useState(false);
+
   const {formatMessage} = useLocalization();
 
+  // function clickHandler() {
+  //   setToggler((toggler) => !toggler);
+  // }
+
   return (
-    <TreeList
-      // id="tree-list"
-      dataSource={soatoData}
-      rootValue={0}
-      keyExpr="id"
-      parentIdExpr="pid"
-      defaultExpandedRowKeys={[1, 2]}
-      showRowLines={true}
-      columnAutoWidth={true}
-      wordWrapEnabled={true}
-    >
-      <SearchPanel visible={true} />
-      <HeaderFilter visible={true} />
-      <Scrolling mode="standard" />
+    <>
+      {/* <Button icon="check" type="success" text="Done" onClick={clickHandler} /> */}
 
-      <Paging defaultPageSize={10} enabled={true} />
-      <Pager
-        showPageSizeSelector={true}
-        showInfo={true}
-        showNavigationButtons={true}
-        allowedPageSizes={[10, 20, 50]}
-        visible={true}
-      />
-
-      <Editing
-        mode="row"
-        allowAdding={true}
-        allowUpdating={true}
-        allowDeleting={true}
-      />
-
-      <Column
-        dataField="territory_name_rus"
-        caption={formatMessage("territory_name_rus")}
-        minWidth={250}
+      <TreeList
+        dataSource={soatoData}
+        rootValue={0}
+        keyExpr="id"
+        parentIdExpr="pid"
+        defaultExpandedRowKeys={[1, 2]}
+        showRowLines={true}
+        columnAutoWidth={true}
+        wordWrapEnabled={true}
+        autoExpandAll={true}
+        focusedRowEnabled={true}
       >
-        <RequiredRule />
-      </Column>
+        <SearchPanel visible={true} />
+        <HeaderFilter visible={true} allowSearch={true} />
+        <FilterRow visible={true} />
+        <Scrolling mode="standard" />
 
-      <Column dataField="code" caption={formatMessage("code")} />
-    </TreeList>
+        <Paging defaultPageSize={15} enabled={true} />
+        <Pager
+          showPageSizeSelector={true}
+          showInfo={true}
+          showNavigationButtons={true}
+          allowedPageSizes={[15, 30, 100]}
+          visible={true}
+        />
+
+        <Editing
+          mode="row"
+          allowAdding={true}
+          allowUpdating={true}
+          allowDeleting={true}
+        />
+
+        <Column
+          dataField="territory_name_rus"
+          caption={formatMessage("territory_name_rus")}
+          minWidth={250}
+        >
+          <RequiredRule />
+        </Column>
+
+        <Column dataField="code" caption={formatMessage("code")} />
+      </TreeList>
+    </>
   );
 };
