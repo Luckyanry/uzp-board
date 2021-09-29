75c672dacb12b591dcd189f7134d46c839cb0738
Andrii Kochmaruk <luckyanry@gmail.com>
2021-09-29 17:52:01 +0200

    url-config.js

--- src/api/url-config.js
+++ src/api/url-config.js
@@ -1,5 +1,7 @@
-const urlAnonymous = "https://uzapi.is.in.ua";
-const urlADauth = "https://uz.is.in.ua";
+// const urlAnonymous = "https://uzapi.is.in.ua";
+// const urlADauth = "https://uz.is.in.ua";
+const urlADauth = "https://tst.is.in.ua:4343";
+const urlAnonymous = "https://tst.is.in.ua:8443";
 const urlBaseParam = "/actions.asp?";
 
 export {urlAnonymous, urlADauth, urlBaseParam};
