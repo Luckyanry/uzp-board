037a4d3f0aada0531978eb4490dce05ea56b74a9
Andrii Kochmaruk <luckyanry@gmail.com>
2021-09-29 18:05:41 +0200

    UAIS saving predeploy version

--- src/api/url-config.js
+++ src/api/url-config.js
@@ -1,7 +1,7 @@
-// const urlAnonymous = "https://uzapi.is.in.ua";
-// const urlADauth = "https://uz.is.in.ua";
-const urlADauth = "https://tst.is.in.ua:4343";
-const urlAnonymous = "https://tst.is.in.ua:8443";
+const urlAnonymous = "https://uzapi.is.in.ua";
+const urlADauth = "https://uz.is.in.ua";
+// const urlAnonymous = "https://tst.is.in.ua:8443";
+// const urlADauth = "https://tst.is.in.ua:4343";
 const urlBaseParam = "/actions.asp?";
 
 export {urlAnonymous, urlADauth, urlBaseParam};
